265b1df3fd451198c981cb7c4d6926e029b989a1
thompson <alfredthompsonovie@gmail.com>
2022-11-12 16:49:52 +0100

    major clear out

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "HomeView",
+  }
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+
+</style>
