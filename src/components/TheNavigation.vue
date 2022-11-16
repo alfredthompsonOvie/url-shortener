22b7598c57d86207b57861b92d75447efc22d0cd
thompson <alfredthompsonovie@gmail.com>
2022-11-16 22:03:57 +0100

    little details css

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -72,7 +72,7 @@ export default {
 	methods: {
 		checkScreen() {
 			this.windowWidth = window.innerWidth;
-			if (this.windowWidth < 700) {
+			if (this.windowWidth < 992) {
 				this.mobile = true;
 				this.mobileMenu = false;
 				return;
@@ -89,7 +89,7 @@ export default {
 header {
 	display: grid;
 	grid-template-columns: 1fr 10fr 1fr;
-	grid-template-rows: 5em;
+	grid-template-rows: 6em;
 	align-items: center;
 }
 .nav__bar {
@@ -127,19 +127,19 @@ header {
 }
 .nav__link--mobile {
 	display: block;
-	padding: .6em 0;
+	padding: 0.6em 0;
 	color: #fff;
 	font-weight: var(--fw-bold);
 }
 .nav__item--mobile + .nav__item--mobile {
-	margin-top: .6em;
+	margin-top: 0.6em;
 }
 .nav__item--mobile:first-child a {
 	padding-top: 0;
 }
 .nav__item--mobile:nth-of-type(4) {
 	border-top: 1px solid var(--GrayishViolet);
-	padding: 1em 0 .6em;
+	padding: 1em 0 0.6em;
 	margin-top: 1.2em;
 }
 .nav__link--mobile.cta {
@@ -147,26 +147,29 @@ header {
 	border-radius: 5em;
 }
 /* larger screen size */
-@media (min-width: 700px) {
+@media (min-width: 992px) {
 	.nav__bar {
 		justify-content: flex-start;
+		align-items: center;
 		gap: 1em;
-		/* border: 1px solid red; */
-
+	}
+	.branding__img {
+		padding-top: .6em;
 	}
 	.nav__list {
 		display: flex;
+		align-items: center;
 		gap: 1em;
-		/* border: 1px solid red; */
 		width: 100%;
 	}
 	.nav__link {
-	padding: 0em;
-
-	color: var(--GrayishViolet);
-}
-.nav__item:nth-of-type(4) {
-	margin-left: auto;
-}
+		padding: 0em;
+		color: var(--GrayishViolet);
+		font-size: 1rem;
+		font-weight: var(--fw-md);
+	}
+	.nav__item:nth-of-type(4) {
+		margin-left: auto;
+	}
 }
 </style>
