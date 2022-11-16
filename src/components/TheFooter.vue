7f626e1591bd3cc310e9301dce4e8727ad43e8e6
thompson <alfredthompsonovie@gmail.com>
2022-11-16 21:10:04 +0100

    footer component

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,91 @@
+<template>
+	<footer class="footer">
+		<div class="footer__contents">
+			<div class="branding--footer">
+				<img
+					src="@/assets/images/logo.svg"
+					alt=""
+					class="branding__img--footer"
+				/>
+			</div>
+			<ul class="footer__nav__list">
+				<li class="nav__item--footer">
+					<a href="#" class="nav__link--footer nav__link__header">Features</a>
+				</li>
+				<li class="nav__item--footer">
+					<a href="#" class="nav__link--footer"> Link Shortening </a>
+				</li>
+				<li class="nav__item--footer">
+					<a href="#" class="nav__link--footer">Branded Links</a>
+				</li>
+				<li class="nav__item--footer">
+					<a href="#" class="nav__link--footer">Analytics</a>
+				</li>
+			</ul>
+			<ul class="footer__nav__list">
+				<li class="nav__item--footer">
+					<a href="#" class="nav__link--footer nav__link__header">Resources</a>
+				</li>
+				<li class="nav__item--footer">
+					<a href="#" class="nav__link--footer">Blog</a>
+				</li>
+				<li class="nav__item--footer">
+					<a href="#" class="nav__link--footer">Developers</a>
+				</li>
+				<li class="nav__item--footer">
+					<a href="#" class="nav__link--footer">Support</a>
+				</li>
+			</ul>
+
+			<ul class="footer__nav__list">
+				<li class="nav__item--footer">
+					<a href="#" class="nav__link--footer nav__link__header">Company</a>
+				</li>
+				<li class="nav__item--footer">
+					<a href="#" class="nav__link--footer">About </a>
+				</li>
+				<li class="nav__item--footer">
+					<a href="#" class="nav__link--footer">Our Team </a>
+				</li>
+				<li class="nav__item--footer">
+					<a href="#" class="nav__link--footer">Careers</a>
+				</li>
+				<li class="nav__item--footer">
+					<a href="#" class="nav__link--footer">Contact</a>
+				</li>
+			</ul>
+			<ul class="social__links">
+				<li>
+					<a href="#"><img src="@/assets/images/icon-facebook.svg" alt="" /></a>
+				</li>
+				<li>
+					<a href="#"><img src="@/assets/images/icon-twitter.svg" alt="" /></a>
+				</li>
+				<li>
+					<a href="#"
+						><img src="@/assets/images/icon-pinterest.svg" alt=""
+					/></a>
+				</li>
+				<li>
+					<a href="#"
+						><img src="@/assets/images/icon-instagram.svg" alt=""
+					/></a>
+				</li>
+			</ul>
+		</div>
+		<div class="attribution">
+			<p>
+				Challenge by
+				<a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+					>Frontend Mentor</a
+				>. Coded by <a href="#">Your Name Here</a>.
+			</p>
+		</div>
+	</footer>
+</template>
+
+<script>
+export default {
+	name: "TheFooter",
+};
+</script>
